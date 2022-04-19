d09a01319f28ac91275602804a19ca37c45c5874
davidshyu <test@testdeMacBook-Pro.local>
2022-04-19 10:22:06 +0800

    Initial commit

---
+++ src/main/java/com.ibm/Hello.java
@@ -0,0 +1,11 @@
+package com.ibm;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("12313");
+
+    }
+
+
+}
---
+++ src/main/kotlin/com/ibm/Hello.kt
@@ -0,0 +1,16 @@
+package com.ibm
+
+fun main() {
+//    println("hello kotlin");
+//    Human().hello();
+
+    val h =Human();
+    h.hello()
+}
+
+
+class Human {
+    fun hello(){
+        println("hello kotlin")
+    }
+}
